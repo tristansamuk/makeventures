3ed3d1ac6822f67af662fbd32c939db4d6406f2e
Tristan Samuk <tsamuk@gmail.com>
2026-03-27 21:51:21 -0400

    Update layout on index page

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Makeventures';
-export const SITE_DESCRIPTION = 'A blog about tinkering with code, IoT, home labs, videography, and whatever else catches my eye.';
+export const SITE_DESCRIPTION = 'Builds, experiments, and field notes from a maker. Code, hardware, home automation, and whatever else catches my eye.';
